4106689d7c56c6d34270debd9bd6aa0395243e89
Ravi Singhal <ravi.singhal21@gmail.com>
2016-03-30 11:16:44 +0530

    sysout

--- twitter-starter/src/main/java/com/hackathon/twitteraccess/TwitterController.java
+++ twitter-starter/src/main/java/com/hackathon/twitteraccess/TwitterController.java
@@ -85,6 +85,7 @@ public class TwitterController {
 
 			@Override
 			public void onStatus(Status status) {
+				System.out.println(status.getText());
 				template.convertAndSend("/topic/tweets", new Tweet(status.getUser().getScreenName()+" : "+status.getText()));
 				
 			}
